ffae90323cf797d4127eadc7ad07291afb56e2ec
Madina-S <madina.saidova@outlook.com>
2017-05-08 23:21:29 +0500

    Create Group in admin panel

---
+++ dashboard/main/createCourse.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once ("ObjectSources/Admin.php");
+require_once ("ObjectSources/User.php");
+use \Student;
+use \User;
+use \Admin;
+session_start();
+
+$admin = $_SESSION['admin'];
+
+require_once ("adminPageTop.html");
+
+require_once ("createCourseContent.html");
+
+require_once ("adminPageBottom.html");
