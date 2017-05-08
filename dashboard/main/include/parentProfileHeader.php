69a07662d1c21234006132e1f9a0142b48d0cb86
Abdulbosid <abdulbosid.kh@gmail.com>
2017-05-08 17:05:52 +0500

    Divided bootstrap into include files and added switching, further changes and bug fixes will be done when I'll go to IUT

--- dashboard/main/include/parentProfileHeader.php
+++ dashboard/main/include/parentProfileHeader.php
@@ -1,4 +1,3 @@
-
 <meta charset="utf-8" />
 <link rel="apple-touch-icon" sizes="76x76" href="http://demos.creative-tim.com/material-dashboard-pro/assets/img/apple-icon.png" />
 <link rel="icon" type="image/png" href="http://demos.creative-tim.com/material-dashboard-pro/assets/img/favicon.png" />
--- dashboard/main/registercourse.php
+++ dashboard/main/registercourse.php
@@ -1,6 +1,6 @@
 <?php
 require_once("DBManager.php");
-
+session_start();
 $selectCourseId = null;
 $selectStudentId = null;
 $selectTime = null;
@@ -42,20 +42,7 @@ if (isset($parent))
 
 
 <head>
-    <meta charset="utf-8" />
-    <link rel="apple-touch-icon" sizes="76x76" href="http://demos.creative-tim.com/material-dashboard-pro/assets/img/apple-icon.png" />
-    <link rel="icon" type="image/png" href="http://demos.creative-tim.com/material-dashboard-pro/assets/img/favicon.png" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    <title>Edu Management System</title>
-    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
-    <meta name="viewport" content="width=device-width" />
-    <!-- Bootstrap core CSS     -->
-    <link href="../../assets/css/bootstrap.min.css" rel="stylesheet" />
-    <!--  Material Dashboard CSS    -->
-    <link href="../../assets/css/material-dashboard.css" rel="stylesheet" />
-    <!--     Fonts and icons     -->
-    <link href="../../assets/css/font-awesome.min.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons" />
+    <?php include "include/parentProfileHeader.php"; ?>
 </head>
 
 <body>
@@ -66,143 +53,14 @@ if (isset($parent))
     Tip 2: you can also add an image using data-image tag
     Tip 3: you can change the color of the sidebar with data-background-color="white | black"
 -->
-        <div class="logo">
-            <a href="#" class="simple-text">
-                Edu System
-            </a>
-        </div>
-        <div class="logo logo-mini">
-            <a href="#" class="simple-text">
-                EMS
-            </a>
-        </div>
-        <div class="sidebar-wrapper">
-            <div class="user">
-                <div class="photo">
-                    <img src="../../assets/img/avatar_default.png" />
-                </div>
-                <div class="info">
-                    <a data-toggle="collapse" href="#collapseExample" class="collapsed">
-                        Brian Cameron
-                        <b class="caret"></b>
-                    </a>
-                    <div class="collapse" id="collapseExample">
-                        <ul class="nav">
-                            <li>
-                                <a href="#">Edit Profile</a>
-                            </li>
-                            <li>
-                                <a href="#">Change Password</a>
-                            </li>
-                            <li>
-                                <a href="#">Log out</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <ul class="nav">
-                <li>
-                    <a href="#">
-                        <i class="material-icons">dashboard</i>
-                        <p>Dashboard</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" >
-                        <i class="material-icons">face</i>
-                        <p>Register kid</p>
-                    </a>
-                </li>
-                <li class="active">
-                    <a href="#">
-                        <i class="material-icons">content_paste</i>
-                        <p>Register to the course</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="leaderboard.php">
-                        <i class="material-icons">account_circle</i>
-                        <p>Track kid performance</p>
-                    </a>
-                </li>
-            </ul>
-        </div>
+        <?php include "include/siteLogo.php"; ?>
+        <?php include "include/profileWrapper.php"; ?>
+
     </div>
     <div class="main-panel">
-        <nav class="navbar navbar-transparent navbar-absolute">
-            <div class="container-fluid">
-                <div class="navbar-minimize">
-                    <button id="minimizeSidebar" class="btn btn-round btn-white btn-fill btn-just-icon">
-                        <i class="material-icons visible-on-sidebar-regular">more_vert</i>
-                        <i class="material-icons visible-on-sidebar-mini">view_list</i>
-                    </button>
-                </div>
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="#"> Profile </a>
-                </div>
-                <div class="collapse navbar-collapse">
-                    <ul class="nav navbar-nav navbar-right">
-                        <li>
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                <i class="material-icons">dashboard</i>
-                                <p class="hidden-lg hidden-md">Dashboard</p>
-                            </a>
-                        </li>
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                <i class="material-icons">notifications</i>
-                                <span class="notification">5</span>
-                                <p class="hidden-lg hidden-md">
-                                    Notifications
-                                    <b class="caret"></b>
-                                </p>
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li>
-                                    <a href="#">Mike John responded to your email</a>
-                                </li>
-                                <li>
-                                    <a href="#">You have 5 new tasks</a>
-                                </li>
-                                <li>
-                                    <a href="#">You're now friend with Andrew</a>
-                                </li>
-                                <li>
-                                    <a href="#">Another Notification</a>
-                                </li>
-                                <li>
-                                    <a href="#">Another One</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                <i class="material-icons">person</i>
-                                <p class="hidden-lg hidden-md">Profile</p>
-                            </a>
-                        </li>
-                        <li class="separator hidden-lg hidden-md"></li>
-                    </ul>
-                    <form class="navbar-form navbar-right" role="search">
-                        <div class="form-group form-search is-empty">
-                            <input type="text" class="form-control" placeholder="Search">
-                            <span class="material-input"></span>
-                        </div>
-                        <button type="submit" class="btn btn-white btn-round btn-just-icon">
-                            <i class="material-icons">search</i>
-                            <div class="ripple-container"></div>
-                        </button>
-                    </form>
-                </div>
-            </div>
-        </nav>
+
+        <?php include "include/parentNavBar.php"; ?>
+
         <div class="content">
             <div class="container-fluid">
                 <div class="row">
@@ -285,45 +143,7 @@ if (isset($parent))
     </div>
 </div>
 </body>
-<!--   Core JS Files   -->
-<script src="../../assets/js/jquery-3.1.1.min.js" type="text/javascript"></script>
-<script src="../../assets/js/jquery-ui.min.js" type="text/javascript"></script>
-<script src="../../assets/js/bootstrap.min.js" type="text/javascript"></script>
-<script src="../../assets/js/material.min.js" type="text/javascript"></script>
-<script src="../../assets/js/perfect-scrollbar.jquery.min.js" type="text/javascript"></script>
-<!-- Forms Validations Plugin -->
-<script src="../../assets/js/jquery.validate.min.js"></script>
-<!--  Plugin for Date Time Picker and Full Calendar Plugin-->
-<script src="../../assets/js/moment.min.js"></script>
-<!--  Charts Plugin -->
-<script src="../../assets/js/chartist.min.js"></script>
-<!--  Plugin for the Wizard -->
-<script src="../../assets/js/jquery.bootstrap-wizard.js"></script>
-<!--  Notifications Plugin    -->
-<script src="../../assets/js/bootstrap-notify.js"></script>
-<!--   Sharrre Library    -->
-<script src="../../assets/js/jquery.sharrre.js"></script>
-<!-- DateTimePicker Plugin -->
-<script src="../../assets/js/bootstrap-datetimepicker.js"></script>
-<!-- Vector Map plugin -->
-<script src="../../assets/js/jquery-jvectormap.js"></script>
-<!-- Sliders Plugin -->
-<script src="../../assets/js/nouislider.min.js"></script>
-<!-- Select Plugin -->
-<script src="../../assets/js/jquery.select-bootstrap.js"></script>
-<!--  DataTables.net Plugin    -->
-<script src="../../assets/js/jquery.datatables.js"></script>
-<!-- Sweet Alert 2 plugin -->
-<script src="../../assets/js/sweetalert2.js"></script>
-<!--	Plugin for Fileupload, full documentation here: http://www.jasny.net/bootstrap/javascript/#fileinput -->
-<script src="../../assets/js/jasny-bootstrap.min.js"></script>
-<!--  Full Calendar Plugin    -->
-<script src="../../assets/js/fullcalendar.min.js"></script>
-<!-- TagsInput Plugin -->
-<script src="../../assets/js/jquery.tagsinput.js"></script>
-<!-- Material Dashboard javascript methods -->
-<script src="../../assets/js/material-dashboard.js"></script>
-<script src="../../assets/js/main.js"></script>
+    <?php include "include/parentProfileFooter.php" ?>
 <script>
     $(document).ready(function(){
         //Listen for a click on selectStudent of the dropdown items
